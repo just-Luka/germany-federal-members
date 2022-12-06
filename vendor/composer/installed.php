219e6e6886ad3f4b4d1e491837cfc7d1d288054e
Mr. Luka <info.tsiklauri@gmail.com>
2022-12-06 19:15:13 +0100

    requirement changes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'm3duza/germany24th_federal_cabinet_members',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '7ff4207008e30c47232ff362be4417a9ddfb883e',
+        'reference' => '1dce6b8265c383601ae24832772bccce47efbacc',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'm3duza/germany24th_federal_cabinet_members' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '7ff4207008e30c47232ff362be4417a9ddfb883e',
+            'reference' => '1dce6b8265c383601ae24832772bccce47efbacc',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
